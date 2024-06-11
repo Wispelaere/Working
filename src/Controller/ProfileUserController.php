743b6fcf93aafeba7ac4c9b28f70fdcf7d6ac8e5
Wispelaere <ibizaa62@gmail.com>
2024-06-11 08:11:08 +0200

    Fix/NavBarModified

--- src/Controller/ProfileUserController.php
+++ src/Controller/ProfileUserController.php
@@ -7,10 +7,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/profil', name: 'profile_')]
+#[Route('/profil')]
 class ProfileUserController extends AbstractController
 {
-    #[Route('/', name: 'index')]
+    #[Route('/', name: 'app_profile')]
     public function index(UsersRepository $usersRepository): Response
     {
         return $this->render('profile_user/index.html.twig', [
