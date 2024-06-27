afe10606de9eb9f07b61e87764b8c822a1d2a1b7
Wispelaere <ibizaa62@gmail.com>
2024-06-27 10:04:40 +0200

    Fix/ModifiedAnnounce

--- src/Repository/AlbumRepository.php
+++ src/Repository/AlbumRepository.php
@@ -2,24 +2,42 @@
 
 namespace App\Repository;
 
-use App\Entity\Announces;
+use App\Entity\Album;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<Announces>
- *
- * @method Announces|null find($id, $lockMode = null, $lockVersion = null)
- * @method Announces|null findOneBy(array $criteria, array $orderBy = null)
- * @method Announces[]    findAll()
- * @method Announces[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @extends ServiceEntityRepository<Album>
  */
-class AnnouncesRepository extends ServiceEntityRepository
+class AlbumRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Announces::class);
+        parent::__construct($registry, Album::class);
     }
 
-    // Add custom query methods here if needed
+    //    /**
+    //     * @return Album[] Returns an array of Album objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('a.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?Album
+    //    {
+    //        return $this->createQueryBuilder('a')
+    //            ->andWhere('a.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
 }
