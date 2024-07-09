50e3f65d45aae82a73d68aa26086dd70c7286509
Wispelaere <ibizaa62@gmail.com>
2024-07-09 23:42:40 +0200

    Fix/MailingAndCssAllPage

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,30 +4,23 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Form\RegistrationFormType;
-use App\Security\UsersAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
-
-
 
 class RegistrationController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, Security $security, EntityManagerInterface $entityManager, MailerInterface $mailer): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
         $user = new Users();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
             $user->setPassword(
                 $userPasswordHasher->hashPassword(
                     $user,
@@ -35,25 +28,10 @@ class RegistrationController extends AbstractController
                 )
             );
 
-            // Generate a token for email confirmation
-            $user->setConfirmationToken(uniqid());
-
             $entityManager->persist($user);
             $entityManager->flush();
 
-            // Send confirmation email
-            $email = (new Email())
-                ->from('noreply@example.com')
-                ->to($user->getEmail())
-                ->subject('Confirmation d\'inscription')
-                ->html($this->renderView(
-                    'emails/registration_confirmation.html.twig',
-                    ['user' => $user]
-                ));
-
-            $mailer->send($email);
-
-            return $this->redirectToRoute('app_home'); // Redirect to home or login page after registration
+            return $this->redirectToRoute('main');
         }
 
         return $this->render('registration/register.html.twig', [
